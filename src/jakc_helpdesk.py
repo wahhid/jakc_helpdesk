349f5d873264bc9db2ee67aae920c034496776d4
wahhid@gmail.com <wahhid@gmail.com>
2014-06-07 22:07:03 +0700

    Fix Error Default priority

--- src/jakc_helpdesk.py
+++ src/jakc_helpdesk.py
@@ -35,8 +35,8 @@ helpdesk_conversation()
     
 class helpdesk_ticket(osv.osv):
     _name = "helpdesk.ticket"
-    _description = "Helpdesk Ticket"
-    
+    _description = "Helpdesk Ticket"        
+        
     def case_close(self, cr, uid, ids, context=None):        
         self.write(cr,uid,ids,{'end_date': datetime.now(),'state':'done'})        
         return True
@@ -59,20 +59,20 @@ class helpdesk_ticket(osv.osv):
         'priority': fields.selection([('1', 'Highest'),('2', 'High'),('3', 'Normal'),('4', 'Low'),('5', 'Lowest'),],'Priority'),
         'technician': fields.many2one('res.users', 'Responsible'),        
         'asset': fields.many2one('asset.assets','Asset'),
-        'message': fields.text('Message'),
+        'description': fields.text('Description'),
         'conversation_ids': fields.one2many('helpdesk.conversation', 'ticket_id', 'Conversation'),
         'start_date': fields.datetime('Start Date', readonly=True),
         'end_date': fields.datetime('End Date', states={'done': [('readonly', True)]}),
         'resolution': fields.text('Resolution'),
         'duration': fields.float('Duration', states={'done': [('readonly', True)]}),
         'active': fields.boolean('Active', required=False),
-        'state': fields.selection([('draft', 'New'),('cancel', 'Cancelled'),('open', 'In Progress'),('pending', 'Pending'),('done', 'Closed')], 'Status', size=16, readonly=True),
+        'state': fields.selection([('draft', 'New'),('cancel', 'Cancelled'),('open', 'In Progress'),('pending', 'Pending'),('done', 'Closed')], 'Status', size=16, readonly=True),        
     }    
     _defaults = {        
         'active': lambda *a: 1,                        
         'state': lambda *a: 'draft',
         'start_date': lambda *a: fields.datetime.now(),
-        'priority': lambda *a: 'Normal',
+        'priority': lambda *a: '3',
     }
     
     def create(self, cr, uid, values, context=None):		                        
